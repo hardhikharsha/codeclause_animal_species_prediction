da1d12dc65c11a7e3ee6508cb8f8c1766d2f9a62
hardhikharsha <139148194+hardhikharsha@users.noreply.github.com>
2023-08-27 20:12:51 +0530

    Update animal_spices_prediction.py

--- animal_spices_prediction.py
+++ animal_spices_prediction.py
@@ -1,1 +1,164 @@
+import keras
+import os
+import shutil
+import numpy as np
+import math
+import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+from platform import python_version
+
+print('Python version:', python_version())
+print('Numpy version:', np.__version__)
+print('Seaborn version:', sns.__version__)
+from distutils.dir_util import copy_tree
+import tensorflow as tf
+print('tensorflow version: ',tf.__version__)
+print('keras version:', keras.__version__)
+
+from keras.layers import Convolution2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+from keras.models import Sequential
+from keras.layers import Dropout
+from keras_preprocessing.image import ImageDataGenerator
+
+#created data set using console
+source='../input/african-wildlife/'
+target='./train_data/'
+shutil.copytree(source, target)
+os.mkdir('test_data')
+
+path="./train_data/"
+for file in os.listdir(path):
+    for image in os.listdir(path+file+'/'):
+        if '.jpg' not in image:
+            os.remove(path+file+'/'+image)
+    os.mkdir('./test_data/'+file)
+
+# create test_data by taking 25% images from data
+
+total_train_images,total_test_images,total_train_classes,total_test_classes=0,0,0,0
+path="./train_data/"
+for file in os.listdir(path):
+    total_train_classes+=1
+    total_images=len(os.listdir(path+file+"/"))
+    test_image_count=(25/100)*total_images #25% for test and 75% for train
+    for i in range(math.ceil(test_image_count)):
+        img=random.choice(os.listdir(path+file+'/'))
+        shutil.move(path+file+'/'+img,'./test_data/'+file+'/')
+        #print(img)
+    print(file,total_images,math.ceil(test_image_count))
+    total_train_images+=(total_images-math.ceil(test_image_count))
+    #print(file,math.ceil(test_image_count))
+print("total train images are : ",total_train_images," and total train classes are : ",total_train_classes)
+
+model = Sequential()
+#inputlayer : apply filters
+model.add(Convolution2D(filters=32, 
+                        kernel_size=(3,3),
+                        strides=(1,1),
+                        padding='same',
+                        activation='relu',
+                   input_shape=(32, 32, 1)))
+
+# pooling layer where we are doing maxpooling
+model.add(MaxPooling2D(pool_size=(2, 2)))
+#adding one more convolution layer for better model
+model.add(Convolution2D(filters=32, 
+                        kernel_size=(3,3),
+                        strides=(1,1),
+                        padding='same', 
+                        activation='relu'
+                      ))
+#adding one more Pooling layer for better model
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+#dropout regularlization
+model.add(Dropout(0.5))
+#layer in which we are converting 2d/3d image to 1d image i.e flattening
+model.add(Flatten())
+# layer: appling relu to give positive output from here our hidden layerrs starts
+model.add(Dense(units=20, activation='relu'))
+#dropout regularlization
+model.add(Dropout(0.5))
+
+# output layer : Since we have to do multi-class classification so we'll apply softmax activation function 
+# we have 4 classes of animals so output layer would have that many neurons.
+model.add(Dense(units=4, activation='softmax'))
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+#url : https://keras.io/api/preprocessing/image/ 
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
+training_set = train_datagen.flow_from_directory(
+        './train_data/',
+        target_size=(32,32),
+        color_mode="grayscale",
+        batch_size=64,
+        class_mode='categorical')
+test_set = test_datagen.flow_from_directory(
+        './test_data/',
+        target_size=(32,32),
+        color_mode="grayscale",
+        batch_size=64,
+        class_mode='categorical')
+
+training_set.class_indices
+
+history = model.fit(
+        training_set,
+        steps_per_epoch=(1125/64),
+        epochs=100,
+        validation_data=test_set,
+        validation_steps=(376/64))
+
+accuracy = history.history['accuracy']
+val_accuracy = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs = range(len(accuracy))
+plt.plot(epochs, accuracy, 'r', label='Training acc')
+plt.plot(epochs, val_accuracy, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.ylabel('accuracy') 
+plt.xlabel('epoch')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'r', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.ylabel('loss') 
+plt.xlabel('epoch')
+plt.legend()
+plt.show()
+
+from keras.preprocessing import image
+test_image = image.load_img("../input/african-wildlife/zebra/001.jpg",target_size=(32,32),color_mode='grayscale')
+test_image 
+
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image,axis=0)
+result = model.predict(test_image)
+
+my_dict=training_set.class_indices
+def get_key(val): 
+    for key, value in my_dict.items(): 
+         if val == value: 
+             return key 
+  
+    return "key doesn't exist"
+
+pred=list(result[0])
+for i in range(len(pred)):
+    if pred[i]!=0:
+        print(get_key(i))
+
 
